3e132facde03c9ff3793341f1f20ffb12b02373d
softprops <d.tangren@gmail.com>
2015-09-13 15:32:51 -0400

    reps dont need to be encodable, organization is not always provided with repo

--- src/rep.rs
+++ src/rep.rs
@@ -1,25 +1,30 @@
 //! A minimal representation of payload information
 
-#[derive(Debug, RustcEncodable, RustcDecodable, Default)]
+#[derive(Debug, RustcDecodable, Default)]
+pub struct Ping {
+  pub hook: Payload
+}
+
+#[derive(Debug, RustcDecodable, Default)]
 pub struct Payload {
   pub sender: Sender,
   pub organization: Option<Organization>,
   pub repository: Option<Repository>
 }
 
-#[derive(Debug, Default, RustcEncodable, RustcDecodable)]
+#[derive(Debug, Default, RustcDecodable)]
 pub struct Sender {
   pub login: String
 }
 
-#[derive(Debug, Default, RustcEncodable, RustcDecodable)]
+#[derive(Debug, Default, RustcDecodable)]
 pub struct Organization {
   pub login: String
 }
 
-#[derive(Debug, Default, RustcEncodable, RustcDecodable)]
+#[derive(Debug, Default, RustcDecodable)]
 pub struct Repository {
   pub name: String,
   pub full_name: String,
-  pub organization: Organization
+  pub organization: Option<Organization>
 }
