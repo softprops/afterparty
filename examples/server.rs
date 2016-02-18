eb3f155417e4d06f2aa89851a3f6c074d3f8585c
softprops <d.tangren@gmail.com>
2016-02-18 18:08:28 -0500

    remove clap

--- examples/server.rs
+++ examples/server.rs
@@ -1,21 +1,15 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-
-extern crate clap;
 extern crate afterparty;
 extern crate hyper;
 
-use afterparty::{Event, Delivery, Hub};
-use clap::App;
+use afterparty::{Delivery, Event, Hub};
+
 use hyper::Server;
 
 pub fn main() {
     env_logger::init().unwrap();
-    let _ = App::new("afterparty")
-        .version(env!("CARGO_PKG_VERSION"))
-        .about("github webhook server")
-        .get_matches();
     let addr = format!("0.0.0.0:{}", 4567);
     let mut hub = Hub::new();
     hub.handle("pull_request", |delivery: &Delivery| {
@@ -23,12 +17,13 @@ pub fn main() {
         match delivery.payload {
             Event::PullRequest { ref action, ref sender, .. } => {
                 println!("sender {} action {}", sender.login, action)
-            },
-            _ => ()
+            }
+            _ => (),
         }
     });
-    let srvc = Server::http(&addr[..]).unwrap()
-        .handle(hub);
+    let srvc = Server::http(&addr[..])
+                   .unwrap()
+                   .handle(hub);
     println!("listening on {}", addr);
     srvc.unwrap();
 }
